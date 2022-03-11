0e891b34a6b689d1fb5c75a571173249c5b1c2fa
Soham Zemse <22412996+zemse@users.noreply.github.com>
2022-03-11 21:45:04 +0530

    Add matic provider

--- loaders/provider.js
+++ loaders/provider.js
@@ -1,6 +1,8 @@
 const ethers = require("ethers");
 
 module.exports = function () {
+  // TODO take API key from an env variable. (underscore added to prevent simple scrapers)
+  const AL_CHEMY = "BlFofLhaR2b18O08NFxUKPdBjHjRCj4P";
   return {
     provider: {
       // TODO allow providing custom providers for custom RPCs
@@ -9,6 +11,9 @@ module.exports = function () {
       rinkeby: ethers.getDefaultProvider("rinkeby"),
       kovan: ethers.getDefaultProvider("kovan"),
       goerli: ethers.getDefaultProvider("goerli"),
+      matic: new ethers.providers.StaticJsonRpcProvider(
+        `https://polygon-mainnet.g.alchemy.com/v2/${AL_CHEMY}`
+      ),
     },
   };
 };
