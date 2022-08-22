7816c201d163de6108ec7f1beabef02ba15bc26d
Soham Zemse <22412996+zemse@users.noreply.github.com>
2022-08-22 22:31:00 +0530

    Add some wallet changes

--- loaders/wallet.js
+++ loaders/wallet.js
@@ -2,25 +2,27 @@ const os = require("os");
 const ethers = require("ethers");
 const fs = require("fs-extra"); // TODO get rid of dependency
 
+let _walletDirPath =
+  process.env.ETHERS_REPL_WALLET_PATH || os.homedir() + "/ethers-repl-wallets";
+
+function listWalletAddresses() {
+  const files = fs.readdirSync(_walletDirPath);
+  const addresses = [];
+  for (const file of files) {
+    const walletJson = fs.readJSONSync(`${_walletDirPath}/${file}`);
+    if (walletJson.address) {
+      addresses.push({
+        address: ethers.utils.getAddress(walletJson.address),
+        jsonFile: `${_walletDirPath}/${file}`,
+      });
+    }
+  }
+  return addresses;
+}
+
 module.exports = function () {
-  let _walletDirPath =
-    process.env.ETHERS_REPL_WALLET_PATH ||
-    os.homedir() + "/ethers-repl-wallets";
   return {
-    listWalletAddresses() {
-      const files = fs.readdirSync(_walletDirPath);
-      const addresses = [];
-      for (const file of files) {
-        const walletJson = fs.readJSONSync(`${_walletDirPath}/${file}`);
-        if (walletJson.address) {
-          addresses.push({
-            address: ethers.utils.getAddress(walletJson.address),
-            jsonFile: `${_walletDirPath}/${file}`,
-          });
-        }
-      }
-      return addresses;
-    },
+    listWalletAddresses,
     async saveWallet(wallet, password) {
       if (!ethers.Wallet.isSigner(wallet)) {
         throw new Error(
@@ -42,11 +44,22 @@ module.exports = function () {
         })
         .catch(console.error);
     },
-    async openWallet(walletStr) {
+    loadWallet(walletStr, password) {
       // TODO allow walletStr to be partial wallet address
-      for (const { address, jsonFile } of this.listWalletAddresses()) {
-        if (address === walletStr) {
-          return fs.readJSONSync(jsonFile);
+      for (const { address, jsonFile } of listWalletAddresses()) {
+        if (address.toLowerCase().includes(walletStr.toLowerCase())) {
+          console.log(`Unlocking ${address}`);
+          const keystore = fs.readJSONSync(jsonFile);
+          try {
+            return ethers.Wallet.fromEncryptedJsonSync(
+              JSON.stringify(keystore),
+              password
+            );
+          } catch (err) {
+            console.error(err);
+            console.log(keystore);
+            return;
+          }
         }
       }
     },
